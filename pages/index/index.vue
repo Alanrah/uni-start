92b2f9bcb21648c93ec35423f40131588d433c7c
18210623224-3u19p <18210623224@163.com>
2024-11-11 17:04:45 +0800

    咸虾米壁纸首页

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -48,17 +48,42 @@
 	    </view>
 	  </view>
 	  <view class="select">
-		  <common-title></common-title>
+		  <common-title>
+			  <template #name>每日推荐</template>
+			  <template #custom>
+				  <view class="date">
+					  <uni-icons type="calendar" size="18" color="#28b389"></uni-icons>
+					  <view class="text">
+						  <uni-dateformat :date="Date.now()" format="dd"></uni-dateformat>
+						  日
+					  </view>
+						
+				  </view>
+			  </template>
+		  </common-title>
 		  <view class="content">
 			  <scroll-view scroll-x>
-				  <view class="box">
+				  <view class="box"  v-for="item in 8">
 					  <image
-					    src="../../common/images/banner1.jpg"
+					    src="../../common/images/preview_small.webp"
 					    mode="aspectFill"
 					  ></image>
 				  </view>
 			  </scroll-view>
 			</view>
+	  </view>
+	  
+	  <view class="theme">
+	  		  <common-title>
+	  			  <template #name>专题精选</template>
+	  			  <template #custom>
+					  <navigator url="" class="more">More+</navigator>
+				  </template>
+	  		  </common-title>
+	  		  <view class="content">
+					<theme-item v-for="item in 8"></theme-item>
+					<theme-item :is-more="true"></theme-item>
+	  			</view>
 	  </view>
   </view>
 </template>
@@ -141,5 +166,54 @@
 	  }
     }
   }
+  .select {
+	  padding-top: 50rpx;
+	  .date{
+		  color: #28b389;
+		  display: flex;
+		  align-items: center;
+		  .text {
+			  margin-left: 15rpx;
+		  }
+	  }
+	  .content{
+		width: 720rpx;
+		margin-top: 30rpx; 
+		margin-left: 30rpx;
+		scroll-view {
+			white-space: nowrap;
+			.box {
+				width: 200rpx;
+				height: 430rpx;
+				display: inline-block;
+				margin-right: 15rpx;
+				image {
+				  width: 100%;
+				  height: 100%;
+				  border-radius: 10rpx;
+				}
+			}
+			.box:last-child{
+				margin-right: 30rpx
+			}
+		}
+	  }
+  }
+	.theme{
+		padding:50rpx 0;
+		.more{
+			font-size: 32rpx;
+			color:#888;
+			
+		}
+		.content{
+			margin-top:30rpx;
+			padding:0 30rpx;
+			// 注意: 网格布局
+			display: grid;
+			gap:15rpx;
+			grid-template-columns: repeat(3,1fr);
+		}
+	}
 }
 </style>
